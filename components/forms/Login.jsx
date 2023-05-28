ddb1b2871b90c596346cd7b62fa7fa5fa109151c
phemzyDeft <oluwadavid081@gmail.com>
2023-05-28 15:01:27 +0100

    update

--- components/forms/Login.jsx
+++ components/forms/Login.jsx
@@ -59,7 +59,7 @@ const Login = () => {
         </div>
       </form>
       <div className='my-4 text-center'>
-        <span className={`${style.withgoogle}`}><Image src={"/authimages/google.png"} width={26} height={26  }/> Sign in with Google</span>
+        <span className={`${style.withgoogle}`}><Image src={"/authimages/google.png"} width={26} height={26} alt='img'/> Sign in with Google</span>
         <span className={`${style.withgoogle} mx-4`} style={{background: '#F7F7F7'}}>
           <Image src={"/authimages/Facebook.png"} width={26} height={26} alt='img'/>
         </span>
